453ba5e17c7886d7925732f8d1bb1f4b12b103bb
Joseph Wakim <jwakim@tower0.stanford.edu>
2020-08-21 00:55:54 -0700

    trivial change: adjusted variable name for clarity

--- tests/test_linalg.py
+++ tests/test_linalg.py
@@ -5,12 +5,12 @@ from chromo.util.linalg import *
 import numpy as np
 
 def test_arbitrary_axis_rotation():
-    r_ind0 = np.array([0, 0, 1]).T
+    r_ind1 = np.array([0, 0, 1]).T
     r_ind_base = np.array([0, 0, 0]).T
     rot_angle = np.pi / 4
 
-    rot_mat_1 = arbitrary_axis_rotation(r_ind0, r_ind_base, rot_angle)
-    rot_mat_2 = arbitrary_axis_rotation(r_ind_base, r_ind0, -rot_angle)
+    rot_mat_1 = arbitrary_axis_rotation(r_ind1, r_ind_base, rot_angle)
+    rot_mat_2 = arbitrary_axis_rotation(r_ind_base, r_ind1, -rot_angle)
     
     print(rot_mat_1)
     print(rot_mat_2)
