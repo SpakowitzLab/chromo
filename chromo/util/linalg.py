BUGFIX: shorten linalg rotation code using scipy

chromo/util/linalg.py
@@ -1,54 +1,43 @@
-"""
-Utility functions for linear algebra calculations
-"""
-import random
-import math as math
-
+"""Utility functions for linear algebra calculations."""
 import numpy as np
+from scipy.spatial.transform import Rotation
 
 
-def arbitrary_axis_rotation(r_ind0, r_ind1, rot_angle):
+def arbitrary_axis_rotation(r0, r1, rot_angle):
     """
-    Generate a transformation matrix for counterclockwise rotation of angle rot_angle about an arbitrary axis from points r_ind0 to r_ind1.
+    Rotate about an axis defined by two points.
+
+    Generate a transformation matrix for counterclockwise (right handed
+    convention) rotation of angle *rot_angle* about an arbitrary axis from
+    points *r0* to *r1*.
+
     Parameters
     ----------
-    r_ind0 : np.array (3 by 1)
-        1D column vector of (x, y, z) coordinates for the first point forming the axis of rotation
-    r_ind1 : np.array (3 by 1)
-        1D column vector of (x, y, z) coordinates for the second point forming the axis of rotation
+    r0 : (3,) array_like
+        First point defining axis to rotate around.
+    r1 : (3,) array_like
+        Second point defining axis to rotate around.
     rot_angle : float
-        Magnitude of the angle of rotation about arbitrary axis
+        Angle of rotation. Positive rotation is counterclockwise when the
+        vector `r1 - r0` is pointing directly out of the blackboard.
+
     Returns
     -------
-    rot_mat : np.array (4 by 4)
-        Homogeneous rotation matrix for rotation about arbitrary axis
+    rot_mat : (4, 4) array_like
+        Homogeneous rotation matrix.
     """
-    
-    # Move end of axis from r_ind0 to origin
     translate_mat = np.array([
-        [1, 0, 0, -r_ind0[0]],
-        [0, 1, 0, -r_ind0[1]],
-        [0, 0, 1, -r_ind0[2]],
+        [1, 0, 0, -r0[0]],
+        [0, 1, 0, -r0[1]],
+        [0, 0, 1, -r0[2]],
         [0, 0, 0, 1]
     ])
-
     inv_translation_mat = translate_mat.copy()
     inv_translation_mat[0:3, 3] *= -1
 
-    dr = r_ind1 - r_ind0
-    r = dr / np.linalg.norm(dr)
-    
-    rot_about_r = np.identity(4)
-    rot_about_r[0,0] = np.cos(rot_angle) + r[0]**2*(1 - np.cos(rot_angle))
-    rot_about_r[0,1] = r[0]*r[1]*(1 - np.cos(rot_angle)) - r[2]*np.sin(rot_angle)
-    rot_about_r[0,2] = r[0]*r[2]*(1 - np.cos(rot_angle)) + r[1]*np.sin(rot_angle)
-    rot_about_r[1,0] = r[0]*r[1]*(1 - np.cos(rot_angle)) + r[2]*np.sin(rot_angle)
-    rot_about_r[1,1] = np.cos(rot_angle) + r[1]**2*(1 - np.cos(rot_angle))
-    rot_about_r[1,2] = r[1]*r[2]*(1 - np.cos(rot_angle)) - r[0]*np.sin(rot_angle)
-    rot_about_r[2,0] = r[0]*r[2]*(1 - np.cos(rot_angle)) - r[1]*np.sin(rot_angle)
-    rot_about_r[2,1] = r[1]*r[2]*(1 - np.cos(rot_angle)) + r[0]*np.sin(rot_angle)
-    rot_about_r[2,2] = np.cos(rot_angle) + r[2]**2*(1 - np.cos(rot_angle))
+    rot_axis = r1 - r0
+    rot_vec = rot_angle * rot_axis/np.linalg.norm(rot_axis)
+    rot_mat = np.identity(4)
+    rot_mat[:3, :3] = Rotation.from_rotvec(rot_vec).as_matrix()
 
-    rot_mat = inv_translation_mat @ rot_about_r @ translate_mat
-
-    return rot_mat
+    return inv_translation_mat @ rot_mat @ translate_mat

tests/test_linalg.py
@@ -1,28 +1,22 @@
-# Test linalg
-
-from chromo.util.linalg import *
+import chromo.util.linalg as clinalg
 
 import numpy as np
 
-def test_arbitrary_axis_rotation():
-    r_ind1 = np.array([0, 0, 1]).T
-    r_ind_base = np.array([0, 0, 0]).T
+
+def test_z_axis_rotation():
+    r0 = np.array([0, 0, 0])
+    r1 = np.array([0, 0, 1])
     rot_angle = np.pi / 4
 
-    rot_mat_1 = arbitrary_axis_rotation(r_ind1, r_ind_base, rot_angle)
-    rot_mat_2 = arbitrary_axis_rotation(r_ind_base, r_ind1, -rot_angle)
-    
-    print(rot_mat_1)
-    print(rot_mat_2)
+    rot_mat = clinalg.arbitrary_axis_rotation(r0, r1, rot_angle)
+    rot_mat_inv_inv = clinalg.arbitrary_axis_rotation(r1, r0, -rot_angle)
 
-    expected_rot_mat_1 = np.identity(4)
-    expected_rot_mat_1[0:2, 0:2] = np.array([[np.cos(rot_angle), -np.sin(rot_angle)],
-        [np.sin(rot_angle), np.cos(rot_angle)]])
+    expected_rot_mat = np.identity(4)
+    expected_rot_mat[0:2, 0:2] = np.array([
+        [np.cos(rot_angle), -np.sin(rot_angle)],
+        [np.sin(rot_angle), np.cos(rot_angle)]
+    ])
 
-    expected_rot_mat_2 = np.identity(4)
-    expected_rot_mat_2[0:2, 0:2] = np.array([[np.cos(-rot_angle), -np.sin(-rot_angle)],
-        [np.sin(-rot_angle), np.cos(-rot_angle)]])
-    
-    assert np.all(np.isclose(rot_mat_1, rot_mat_2))
-    assert np.all(rot_mat_1 == expected_rot_mat_2)
-    assert np.all(rot_mat_2 == expected_rot_mat_2)
+    assert np.all(np.isclose(rot_mat, rot_mat_inv_inv))
+    assert np.all(np.isclose(rot_mat, expected_rot_mat))
+    assert np.all(np.isclose(rot_mat_inv_inv, expected_rot_mat))