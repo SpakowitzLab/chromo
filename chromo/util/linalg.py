a33eb508cb6a651ee3f69ce5fc99355532c6f539
Joseph Wakim <jwakim@tower0.stanford.edu>
2020-08-21 00:45:08 -0700

    updated arbitrary axis rotation and test

--- chromo/util/linalg.py
+++ chromo/util/linalg.py
@@ -5,6 +5,7 @@ import random
 import math as math
 
 import numpy as np
+from scipy.spatial.transform import Rotation as R
 
 
 def arbitrary_axis_rotation(r_ind0, r_ind1, rot_angle):
@@ -12,58 +13,43 @@ def arbitrary_axis_rotation(r_ind0, r_ind1, rot_angle):
     Generate a transformation matrix for counterclockwise rotation of angle rot_angle about an arbitrary axis from points r_ind0 to r_ind1.
     Parameters
     ----------
-    r_ind0:         (3, 1) np.array
-                    1D column vector of (x, y, z) coordinates for the first point forming the axis of rotation
-    r_ind1:         (3, 1) np.array
-                    1D column vector of (x, y, z) coordinates for the second point forming the axis of rotation
-    rot_angle:      float
-                    Magnitude of the angle of rotation about arbitrary axis
+    r_ind0 : np.array (3 by 1)
+        1D column vector of (x, y, z) coordinates for the first point forming the axis of rotation
+    r_ind1 : np.array (3 by 1)
+        1D column vector of (x, y, z) coordinates for the second point forming the axis of rotation
+    rot_angle : float
+        Magnitude of the angle of rotation about arbitrary axis
     Returns
     -------
-    rot_matrix:     (4, 4) np.array
-                    Homogeneous rotation matrix for rotation about arbitrary axis
+    rot_mat : np.array (4 by 4)
+        Homogeneous rotation matrix for rotation about arbitrary axis
     """
+    
+    # Move end of axis from r_ind0 to origin
     translate_mat = np.array([
-        [1, 0, 0, -r_ind1[0]],
-        [0, 1, 0, -r_ind1[1]],
-        [0, 0, 1, -r_ind1[2]],
+        [1, 0, 0, -r_ind0[0]],
+        [0, 1, 0, -r_ind0[1]],
+        [0, 0, 1, -r_ind0[2]],
         [0, 0, 0, 1]
     ])
 
     inv_translation_mat = translate_mat.copy()
     inv_translation_mat[0:3, 3] *= -1
 
-    # Calculate the length of the projections to point ind0 on yz plane.
-    proj_len_yz = math.sqrt(r_ind0[1]**2 + r_ind0[2]**2)
-    full_length = math.sqrt(r_ind0[0]**2 + r_ind0[1]**2 + r_ind0[2]**2)
-    # rotate the vector ind0 into the xz plane
-    if np.isclose(proj_len_yz, 0):
-        rot_mat_x = np.identity(4)
-    else:
-        rot_mat_x = np.identity(4)
-        rot_mat_x[1:3, 1:3] = np.array([
-            [r_ind0[2] / proj_len_yz, -r_ind0[1] / proj_len_yz],
-            [r_ind0[1] / proj_len_yz, r_ind0[2] / proj_len_yz]
-        ])
-    # rotating by the same angle in the opposite direction is the inverse
-    inv_rot_mat_x = rot_mat_x.copy()
-    inv_rot_mat_x[1, 2] = -rot_mat_x[1, 2]
-    inv_rot_mat_x[2, 1] = -rot_mat_x[2, 1]
-    # now rotate around the y-axis such that the ind0 vector becomes the z-axis
-    rot_mat_y = np.identity(4)
-    if not np.isclose(proj_len_yz, 0):
-        rot_mat_y[0, 0] = proj_len_yz / full_length
-        rot_mat_y[0, 2] = -r_ind0[0] / full_length
-        rot_mat_y[2, 0] = r_ind0[0] / full_length
-        rot_mat_y[2, 2] = proj_len_yz / full_length
-    inv_rot_mat_y = rot_mat_y.copy()
-    inv_rot_mat_y[0, 2] = -rot_mat_y[0, 2]
-    inv_rot_mat_y[2, 0] = -rot_mat_y[2, 0]
-    rot_mat_z = np.identity(4)  # now rotate about z
-    rot_mat_z[0:2, 0:2] = np.array([
-        [np.cos(rot_angle), -np.sin(rot_angle)], 
-        [np.sin(rot_angle), np.cos(rot_angle)]
-    ])
-    rot_matrix = inv_translation_mat @ inv_rot_mat_x @ inv_rot_mat_y \
-        @ rot_mat_z @ rot_mat_y @ rot_mat_x @ translate_mat
-    return rot_matrix
+    dr = r_ind1 - r_ind0
+    r = dr / np.linalg.norm(dr)
+    
+    rot_about_r = np.identity(4)
+    rot_about_r[0,0] = np.cos(rot_angle) + r[0]**2*(1 - np.cos(rot_angle))
+    rot_about_r[0,1] = r[0]*r[1]*(1 - np.cos(rot_angle)) - r[2]*np.sin(rot_angle)
+    rot_about_r[0,2] = r[0]*r[2]*(1 - np.cos(rot_angle)) + r[1]*np.sin(rot_angle)
+    rot_about_r[1,0] = r[0]*r[1]*(1 - np.cos(rot_angle)) + r[2]*np.sin(rot_angle)
+    rot_about_r[1,1] = np.cos(rot_angle) + r[1]**2*(1 - np.cos(rot_angle))
+    rot_about_r[1,2] = r[1]*r[2]*(1 - np.cos(rot_angle)) - r[0]*np.sin(rot_angle)
+    rot_about_r[2,0] = r[0]*r[2]*(1 - np.cos(rot_angle)) - r[1]*np.sin(rot_angle)
+    rot_about_r[2,1] = r[1]*r[2]*(1 - np.cos(rot_angle)) + r[0]*np.sin(rot_angle)
+    rot_about_r[2,2] = np.cos(rot_angle) + r[2]**2*(1 - np.cos(rot_angle))
+
+    rot_mat = inv_translation_mat @ rot_about_r @ translate_mat
+
+    return rot_mat
