86b8378719f2794ba081097936c11870c8a112fd
Joseph Wakim <jwakim@tower0.stanford.edu>
2020-08-21 17:38:47 -0700

    CODE: methods for end-pivot move with test

--- chromo/mc/mc_move.py
+++ chromo/mc/mc_move.py
@@ -3,21 +3,30 @@ Routines for generating Monte Carlo moves of varying types
 """
 import numpy as np
 import math as math
-from mc.calc_density import calc_density
+from chromo.mc.calc_density import calc_density
+from chromo.util.bead_selection import *
+from chromo.util.linalg import *
 
 
-def mc_move(polymer, epigenmark, density, num_epigenmark, num_polymers, mcmove, mc_move_type, field):
+def mc_move(polymer, epigenmark, density, num_epigenmark, num_polymers,
+            mcmove, mc_move_type, field):
 
     # MC move type 0: Crank-shaft move
     if mc_move_type == 0:
         for i_move in range(mcmove[mc_move_type].num_per_cycle):
             for i_poly in range(num_polymers):
-                crank_shaft_move(polymer, epigenmark, density, num_epigenmark, i_poly, mcmove, field)
+                crank_shaft_move(polymer, epigenmark, density,
+                                 num_epigenmark, i_poly, mcmove, field)
                 mcmove[mc_move_type].num_attempt += 1
 
     # MC move type 1: End pivot move
     elif mc_move_type == 1:
-        pass
+        window_size = mcmove[mc_move_type].amp_bead
+        for i_move in range(mcmove[mc_move_type].num_per_cycle):
+            for i_poly in range(num_polymers):
+                end_pivot_move(polymer, epigenmark, density,
+                                 num_epigenmark, i_poly, mcmove, field, window_size)
+                mcmove[mc_move_type].num_attempt += 1
 
     # MC move type 2: Slide move
     elif mc_move_type == 2:
@@ -27,86 +36,11 @@ def mc_move(polymer, epigenmark, density, num_epigenmark, num_polymers, mcmove, 
     elif mc_move_type == 3:
         pass
 
-
     # MC move type 4: Epigenetic protein binding move
     elif mc_move_type == 4:
         pass
 
     return
-
-
-def crank_shaft_move(polymer, epigenmark, density, num_epigenmark, i_poly, mcmove, field):
-
-    # Select ind0 and indf for the crank-shaft move
-    delta_ind = min(np.random.randint(2, mcmove[0].amp_bead), polymer[i_poly].num_beads)
-    ind0 = np.random.randint(polymer[i_poly].num_beads - delta_ind + 1)
-    indf = ind0 + delta_ind
-
-    # Generate the rotation matrix and vector around the vector between bead ind0 and indf
-    rot_angle = mcmove[0].amp_move * (np.random.rand() - 0.5)
-
-    if ind0 == (indf + 1):
-        delta_t3 = polymer[i_poly].t3_poly[ind0, :]
-    else:
-        delta_t3 = polymer[i_poly].r_poly[indf - 1, :] - polymer[i_poly].r_poly[ind0, :]
-        delta_t3 /= np.linalg.norm(delta_t3)
-
-    r_ind0 = polymer[i_poly].r_poly[ind0, :]
-
-    rot_matrix = np.zeros((3, 3), 'd')
-
-    rot_matrix[0, 0] = delta_t3[0] ** 2. + (delta_t3[1] ** 2 + delta_t3[2] ** 2) * math.cos(rot_angle)
-    rot_matrix[0, 1] = delta_t3[0] * delta_t3[1] * (1 - math.cos(rot_angle)) - delta_t3[2] * math.sin(rot_angle)
-    rot_matrix[0, 2] = delta_t3[0] * delta_t3[2] * (1 - math.cos(rot_angle)) + delta_t3[1] * math.sin(rot_angle)
-
-    rot_matrix[1, 0] = delta_t3[0] * delta_t3[1] * (1 - math.cos(rot_angle)) + delta_t3[2] * math.sin(rot_angle)
-    rot_matrix[1, 1] = delta_t3[1] ** 2. + (delta_t3[0] ** 2 + delta_t3[2] ** 2) * math.cos(rot_angle)
-    rot_matrix[1, 2] = delta_t3[1] * delta_t3[2] * (1 - math.cos(rot_angle)) - delta_t3[0] * math.sin(rot_angle)
-
-    rot_matrix[2, 0] = delta_t3[0] * delta_t3[2] * (1 - math.cos(rot_angle)) - delta_t3[1] * math.sin(rot_angle)
-    rot_matrix[2, 1] = delta_t3[1] * delta_t3[2] * (1 - math.cos(rot_angle)) + delta_t3[0] * math.sin(rot_angle)
-    rot_matrix[2, 2] = delta_t3[2] ** 2. + (delta_t3[0] ** 2 + delta_t3[1] ** 2) * math.cos(rot_angle)
-
-    rot_vector = np.cross(r_ind0, delta_t3) * math.sin(rot_angle)
-    rot_vector[0] += (r_ind0[0] * (1 - delta_t3[0] ** 2)
-                      - delta_t3[0]*(r_ind0[1] * delta_t3[1] + r_ind0[2] * delta_t3[2])) * (1 - math.cos(rot_angle))
-    rot_vector[1] += (r_ind0[1] * (1 - delta_t3[1] ** 2)
-                      - delta_t3[1]*(r_ind0[0] * delta_t3[0] + r_ind0[2] * delta_t3[2])) * (1 - math.cos(rot_angle))
-    rot_vector[2] += (r_ind0[2] * (1 - delta_t3[2] ** 2)
-                      - delta_t3[2]*(r_ind0[0] * delta_t3[0] + r_ind0[1] * delta_t3[1])) * (1 - math.cos(rot_angle))
-
-    # Generate the trial positions and orientations
-
-    r_poly_trial = np.zeros((indf - ind0, 3), 'd')
-    t3_poly_trial = np.zeros((indf - ind0, 3), 'd')
-    for i_bead in range(ind0, indf):
-        r_poly_trial[i_bead - ind0, :] = rot_vector + np.matmul(rot_matrix, polymer[i_poly].r_poly[i_bead, :])
-        t3_poly_trial[i_bead - ind0, :] = np.matmul(rot_matrix, polymer[i_poly].t3_poly[i_bead, :])
-
-    # Calculate the change in energy
-    density_poly, index_xyz = calc_density(polymer[i_poly].r_poly[ind0:indf, :], polymer[i_poly].epigen_bind,
-                                           num_epigenmark, ind0, indf, field)
-    density_poly_trial, index_xyz_trial = calc_density(r_poly_trial, polymer[i_poly].epigen_bind,
-                                                       num_epigenmark, ind0, indf, field)
-    delta_density_poly_total = np.concatenate((density_poly_trial, -density_poly))
-    delta_index_xyz_total = np.concatenate((index_xyz_trial, index_xyz)).astype(int)
-    delta_density, delta_index_xyz = combine_repeat(delta_density_poly_total, delta_index_xyz_total)
-
-    delta_energy_epigen = 0
-    for i_epigen in range(num_epigenmark):
-        delta_energy_epigen += 0.5 * epigenmark[i_epigen].int_energy * np.sum(
-            (delta_density[:, i_epigen + 1] + density[delta_index_xyz, i_epigen + 1]) ** 2
-            - density[delta_index_xyz, i_epigen + 1] ** 2)
-
-    delta_energy_poly = calc_delta_energy_poly(r_poly_trial, t3_poly_trial, polymer, i_poly, ind0, indf)
-    delta_energy = delta_energy_poly + delta_energy_epigen
-
-    # Determine acceptance of trial based on Metropolis criterion
-    if np.random.rand() < math.exp(-delta_energy):
-        polymer[i_poly].r_poly[ind0:indf, :] = r_poly_trial
-        polymer[i_poly].t3_poly[ind0:indf, :] = t3_poly_trial
-        density[delta_index_xyz, :] += delta_density
-        mcmove[0].num_success += 1
 
 
 def calc_delta_energy_poly(r_poly_trial, t3_poly_trial, polymer, i_poly, ind0, indf):
@@ -196,3 +130,162 @@ def combine_repeat(a, idx):
     idx_combine = a_with_index[:, 0].astype(int)
 
     return a_combine, idx_combine
+
+
+def assess_energy_change(polymer, epigenmark, density, num_epigenmark, i_poly, ind0, indf, field, r_poly_trial, t3_poly_trial):
+    """
+    Assess energy change and acceptance of MC move
+    """
+   
+    density_poly, index_xyz = calc_density(polymer[i_poly].r_poly[ind0:indf, :], polymer[i_poly].epigen_bind,
+                                           num_epigenmark, ind0, indf, field)
+    density_poly_trial, index_xyz_trial = calc_density(r_poly_trial, polymer[i_poly].epigen_bind,
+                                                       num_epigenmark, ind0, indf, field)
+    delta_density_poly_total = np.concatenate((density_poly_trial, -density_poly))
+    delta_index_xyz_total = np.concatenate((index_xyz_trial, index_xyz)).astype(int)
+    delta_density, delta_index_xyz = combine_repeat(delta_density_poly_total, delta_index_xyz_total)
+
+    delta_energy_epigen = 0
+    for i_epigen in range(num_epigenmark):
+        delta_energy_epigen += 0.5 * epigenmark[i_epigen].int_energy * np.sum(
+            (delta_density[:, i_epigen + 1] + density[delta_index_xyz, i_epigen + 1]) ** 2
+            - density[delta_index_xyz, i_epigen + 1] ** 2)
+
+    delta_energy_poly = calc_delta_energy_poly(r_poly_trial, t3_poly_trial, polymer, i_poly, ind0, indf)
+    delta_energy = delta_energy_poly + delta_energy_epigen
+
+    return delta_index_xyz, delta_density, delta_energy
+
+
+def assess_move_acceptance(polymer, i_poly, ind0, indf, density, mcmove, r_poly_trial, t3_poly_trial, delta_index_xyz, delta_density, delta_energy):
+    """
+    Determine move acceptance based on Metropolis criterion
+    """
+    if delta_energy < 0:    # Favorable move
+        prob = 1 
+    else:                   # Unfavorable move
+        try:
+            prob = math.exp(-delta_energy) 
+        except OverflowError:
+            prob = 0 
+    if np.random.rand() < prob:
+        polymer[i_poly].r_poly[ind0:indf, :] = r_poly_trial
+        polymer[i_poly].t3_poly[ind0:indf, :] = t3_poly_trial
+        density[delta_index_xyz, :] += delta_density
+        mcmove[1].num_success += 1
+
+    return
+
+
+def crank_shaft_move(polymer, epigenmark, density, num_epigenmark, i_poly, mcmove, field):
+    # Select ind0 and indf for the crank-shaft move
+    delta_ind = min(np.random.randint(2, mcmove[0].amp_bead), polymer[i_poly].num_beads)
+    ind0 = np.random.randint(polymer[i_poly].num_beads - delta_ind + 1)
+    indf = ind0 + delta_ind
+
+    # Generate the rotation matrix and vector around the vector between bead ind0 and indf
+    rot_angle = mcmove[0].amp_move * (np.random.rand() - 0.5)
+
+    if ind0 == (indf + 1):
+        delta_t3 = polymer[i_poly].t3_poly[ind0, :]
+    else:
+        delta_t3 = polymer[i_poly].r_poly[indf - 1, :] - polymer[i_poly].r_poly[ind0, :]
+        delta_t3 /= np.linalg.norm(delta_t3)
+
+    r_ind0 = polymer[i_poly].r_poly[ind0, :]
+
+    rot_matrix = np.zeros((3, 3), 'd')
+
+    rot_matrix[0, 0] = delta_t3[0] ** 2. + (delta_t3[1] ** 2 + delta_t3[2] ** 2) * math.cos(rot_angle)
+    rot_matrix[0, 1] = delta_t3[0] * delta_t3[1] * (1 - math.cos(rot_angle)) - delta_t3[2] * math.sin(rot_angle)
+    rot_matrix[0, 2] = delta_t3[0] * delta_t3[2] * (1 - math.cos(rot_angle)) + delta_t3[1] * math.sin(rot_angle)
+
+    rot_matrix[1, 0] = delta_t3[0] * delta_t3[1] * (1 - math.cos(rot_angle)) + delta_t3[2] * math.sin(rot_angle)
+    rot_matrix[1, 1] = delta_t3[1] ** 2. + (delta_t3[0] ** 2 + delta_t3[2] ** 2) * math.cos(rot_angle)
+    rot_matrix[1, 2] = delta_t3[1] * delta_t3[2] * (1 - math.cos(rot_angle)) - delta_t3[0] * math.sin(rot_angle)
+
+    rot_matrix[2, 0] = delta_t3[0] * delta_t3[2] * (1 - math.cos(rot_angle)) - delta_t3[1] * math.sin(rot_angle)
+    rot_matrix[2, 1] = delta_t3[1] * delta_t3[2] * (1 - math.cos(rot_angle)) + delta_t3[0] * math.sin(rot_angle)
+    rot_matrix[2, 2] = delta_t3[2] ** 2. + (delta_t3[0] ** 2 + delta_t3[1] ** 2) * math.cos(rot_angle)
+
+    rot_vector = np.cross(r_ind0, delta_t3) * math.sin(rot_angle)
+    rot_vector[0] += (r_ind0[0] * (1 - delta_t3[0] ** 2)
+                      - delta_t3[0]*(r_ind0[1] * delta_t3[1] + r_ind0[2] * delta_t3[2])) * (1 - math.cos(rot_angle))
+    rot_vector[1] += (r_ind0[1] * (1 - delta_t3[1] ** 2)
+                      - delta_t3[1]*(r_ind0[0] * delta_t3[0] + r_ind0[2] * delta_t3[2])) * (1 - math.cos(rot_angle))
+    rot_vector[2] += (r_ind0[2] * (1 - delta_t3[2] ** 2)
+                      - delta_t3[2]*(r_ind0[0] * delta_t3[0] + r_ind0[1] * delta_t3[1])) * (1 - math.cos(rot_angle))
+
+    # Generate the trial positions and orientations
+
+    r_poly_trial = np.zeros((indf - ind0, 3), 'd')
+    t3_poly_trial = np.zeros((indf - ind0, 3), 'd')
+    for i_bead in range(ind0, indf):
+        r_poly_trial[i_bead - ind0, :] = rot_vector + np.matmul(rot_matrix, polymer[i_poly].r_poly[i_bead, :])
+        t3_poly_trial[i_bead - ind0, :] = np.matmul(rot_matrix, polymer[i_poly].t3_poly[i_bead, :])
+
+    delta_index_xyz, delta_density, delta_energy = assess_energy_change(polymer, epigenmark, density, num_epigenmark, i_poly, ind0, indf, field, r_poly_trial, t3_poly_trial)
+    assess_move_acceptance(polymer, i_poly, ind0, indf, density, mcmove, r_poly_trial, t3_poly_trial, delta_index_xyz, delta_density, delta_energy)
+
+
+def end_pivot_move(polymer, epigenmark, density, num_epigenmark, i_poly, mcmove, field, window_size):
+    """
+    Randomly rotate segment from one end of the polymer. 
+
+    Parameters
+    ----------
+    polymer : Polymer
+        Object containing polymer properties
+    epigenmark : epigenmark object
+        Object contianing epigenetic mark properties
+    density : float
+        Mass density of the polymer
+    num_epigenmark : int
+        Number of epigenetic marks included in the simulation
+    i_poly : int
+        Index of individual polymer in polymer object
+    mcmove : mcmove object
+        Object describing characteristics of the MC move (e.g. amplitude)
+    field : Field
+        Object describing properties of the energy field
+    window_size : int
+        Maximum number of nucleosomes to roate in any one MC move
+    """
+    
+    active_poly = polymer[i_poly]   # isolate affected by move
+    active_move = mcmove[1]         # isolate active move
+
+    rot_angle = active_move.amp_move * (np.random.rand() - 0.5)
+    num_beads = active_poly.num_beads
+    select_window = num_beads - 1
+    all_beads = np.arange(0, num_beads, 1)
+
+    if np.random.randint(0, 2) == 0:    # rotate LHS of polymer
+        ind0 = 0
+        pivot_point = indf = select_bead_from_left(select_window, num_beads)
+        base_point = pivot_point + 1
+    else:                               # rotate RHS of polymer
+        pivot_point = ind0 = select_bead_from_right(select_window, num_beads)
+        indf = num_beads-1
+        base_point = pivot_point - 1
+    
+    r_points = np.ones((4, indf-ind0+1)
+    r_point[0:3, :] = active_poly.r_poly[ind0:indf+1, :].T
+    t3_points = np.ones((4, indf-ind0+1))
+    t3_points[0:3, :] = active_poly.t3_poly[ind0:indf+1, :].T
+    
+    r_pivot_point = active_poly.r_poly[pivot_point, :]
+    r_base_point = active_poly.r_poly[base_point, :]
+    
+    rot_matrix = arbitrary_axis_rotation(r_pivot_point, r_base_point, rot_angle)
+    r_poly_trial = np.matmul(rot_matrix, r_points)      # generate trial coordinates
+    r_poly_trial = r_poly_trial[0:3,:].T
+    t3_poly_trial = np.matmul(rot_matrix, t3_points)    # generate trial tangents
+    t3_poly_trial = t3_poly_trial[0:3,:].T
+            
+    # assess move by Metropolis Criterion
+    delta_index_xyz, delta_density, delta_energy = assess_energy_change(polymer, epigenmark, density, num_epigenmark, i_poly, ind0, indf+1, field, r_poly_trial, t3_poly_trial)
+    assess_move_acceptance(polymer, i_poly, ind0, indf+1, density, mcmove, r_poly_trial, t3_poly_trial, delta_index_xyz, delta_density, delta_energy)
+
+    return
+
---
+++ tests/test_end_pivot_move.py
@@ -0,0 +1,14 @@
+
+from chromo.mc.mc_move import *
+import inputs.chromo_for_testing_mc_moves
+from chromo.mc.mc_sim import *
+
+import numpy as np
+
+def test_end_pivot_move():
+	
+	# Generate objects
+	inputs.chromo_for_testing_mc_moves.main()
+
+	assert True
+
