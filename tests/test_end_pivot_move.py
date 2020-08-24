54b765902045f7a2a0578e6fa415571a783566bb
Joseph Wakim <jwakim@tower0.stanford.edu>
2020-08-23 17:08:16 -0700

    began moving inputs into end-pivot test

--- tests/test_end_pivot_move.py
+++ tests/test_end_pivot_move.py
@@ -6,7 +6,19 @@ from chromo.mc.mc_sim import *
 import numpy as np
 
 def test_end_pivot_move():
-	
+
+    num_polymers = 1
+    length_bead = 55
+    num_epigenmark = 2
+    length_box_x = 3
+    num_bins_x = 3
+    length_box_y = 3 
+    num_bins_y = 3
+    length_box_z = 3 
+    num_bins_z = 3
+    num_save_mc = 10
+    num_mc_steps = 1000
+    
 	# Generate objects
 	inputs.chromo_for_testing_mc_moves.main()
 
