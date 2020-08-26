91c9d38cce6717b8167f65cb72107f11b34ad04d
Bruno Beltran <brunobeltran0@gmail.com>
2020-08-25 22:28:51 -0700

    CODE: continue decoupling....

--- chromo/Polymer.py
+++ chromo/Polymer.py
@@ -5,37 +5,57 @@ Creates a polymer with a defined length and discretization.
 """
 from pathlib import Path
 from enum import Enum
-from dataclasses import dataclass
-from abc import ABC
+import dataclasses
 
 import numpy as np
 import pandas as pd
-from .util.init_cond import init_cond
 from .util.find_parameters import find_parameters
 
 
-@dataclass
+@dataclasses.dataclass
 class Epigenmark:
     """Information about the chemical properties of an epigenetic mark."""
 
+    name: str
     bind_energy: float
     interaction_energy: float
     chemical_potential: float
 
 
-class State(Enum, ABC):
+class State:
     """Defines the epigenetic state of a given bead for a single mark."""
 
     pass
 
 
-class HP1_State(State):
+class HP1_State(State, Enum):
     """HP1 can be bound to none, either, or both nucleosome tails."""
 
     NONE = 0
     LBOUND = 1
     RBOUND = 2
     BOTH = 3
+
+
+def make_epi_collection(epigenmarks):
+    """
+    Takes a sequence of epigenetic marks and returns a summary DataFrame.
+
+    Parameters
+    ----------
+    epigenmarks : Sequence[Epigenmark]
+        The epigenetic marks to be summarized.
+
+    Returns
+    -------
+    pd.DataFrame
+        Columns are the properties of Epigenmark.
+    """
+    df = pd.DataFrame(columns=['name', 'bind_energy', 'interaction_energy',
+                               'chemical_potential'])
+    for mark in epigenmarks:
+        df = df.append(dataclasses.asdict(mark), ignore_index=True)
+    return df
 
 
 class Polymer:
@@ -56,10 +76,13 @@ class Polymer:
     objects.
     """
 
-    def __init__(self, r, t_3, t_2, epigenmarks, states, length_per_bead):
+    def __init__(self, name, r, t_3, t_2, epigenmarks, states,
+                 length_per_bead):
         """
         Parameters
         ----------
+        name : str
+            A name for convenient repr. Should be a valid filename.
         r : (N, 3) array_like of float
             The positions of each bead.
         t_3 : (N, 3) array_like of float
@@ -69,51 +92,75 @@ class Polymer:
         epigenmarks : (M, ) Sequence[Epigenmark]
             Information about the chemical properties of each of the epigenetic
             marks being tracked.
-        states : (N, M) array_like of int
+        states : (N, M) array_like of State
             State of each of the M epigenetic marks being tracked for each
             bead.
         length_per_bead : float
             How many Kuhn lengths of polymer are simulated by each bead.
         """
+        self.name = name
         self.r = r
         self.t_3 = t_3
         self.t_2 = t_2
-        self.epigenmarks = epigenmarks
+        self.epigenmarks = make_epi_collection(epigenmarks)
         self.states = states
         self.length_per_bead = length_per_bead
+        # Load the polymer parameters for the conformational energy
+        self.sim_type, self.eps_bend, self.eps_par, self.eps_perp, \
+            self.gamma, self.eta = find_parameters(length_per_bead)
 
-    def from_files(*, r_file=None, t3_file=None, t2_file=None, mark_file=None,
-                   states_file=None, lengths_file=None):
+    @classmethod
+    def from_files(cls, *, r=None, t_3=None, t_2=None, marks=None, states=None,
+                   lengths=None):
         """
         Instantiate a Polymer from input files.
 
         Each of the r, t_3, t_2, epigenmarks, states, and length_per_bead
         arrays requires a separate input file.
         """
-        r = pd.read_csv(r_file, header=None) if r_file is not None else None
-        t_3 = pd.read_csv(t3_file, header=None) if t3_file is not None else None
-        t_2 = pd.read_csv(t2_file, header=None) if t2_file is not None else None
-        states = pd.read_csv(states_file, header=None) if states_file is not None else None
+        r = pd.read_csv(r, header=None) if r is not None else None
+        t_3 = pd.read_csv(t_3, header=None) if t_3 is not None else None
+        t_2 = pd.read_csv(t_2, header=None) if t_2 is not None else None
+        states = pd.read_csv(states) if states is not None else None
+        marks = pd.read_csv(r, header=None) if r is not None else None
+        return cls(r, t_3, t_2, marks, states, lengths)
 
-        # Initialize the conformation, protein binding state, and the sequence of epigenetic marks
-        self.r_poly, self.t3_poly, self.epigen_bind = init_cond(length_bead, 1, self.num_beads,
-                                                                num_epigenmark, from_file, self.input_dir)
+    @classmethod
+    def straight_line_in_x(cls, num_beads, length_per_bead):
+        r = np.zeros((num_beads, 3))
+        r[:, 0] = length_per_bead * np.arange(num_beads)
+        t_3 = np.zeros((num_beads, 3))
+        t_3[:, 0] = 1
+        t_2 = np.zeros((num_beads, 3))
+        t_2[:, 1] = 1
+        marks = None
+        states = None
+        return cls(r, t_3, t_2, marks, states, length_per_bead)
 
-        # Load the polymer parameters for the conformational energy
-        self.sim_type, self.eps_bend, self.eps_par, self.eps_perp, self.gamma, self.eta = find_parameters(length_bead)
+    def write_r(self, prefix, **kwargs):
+        r_file = Path(str(prefix) + self.name + '_r.csv')
+        np.savetxt(r_file, self.r, delimiter=',', **kwargs)
 
-        self.sequence = np.zeros((self.num_beads, num_epigenmark), 'd')
-        for epigenmark_count in range(1, num_epigenmark + 1):
-            seq_file = self.input_dir / Path("chromo" + str(self.polymer_count) + "seq" + str(epigenmark_count))
-            if seq_file.exists():
-                self.sequence[:, epigenmark_count - 1] = np.loadtxt(seq_file, delimiter=',')
-            else:
-                print(f"Sequence file does not exist for chromosome {self.polymer_count}")
-                exit()
+    def write_t2(self, prefix, **kwargs):
+        t2_file = Path(str(prefix) + self.name + '_t2.csv')
+        np.savetxt(t2_file, self.t2, delimiter=',', **kwargs)
+
+    def write_t3(self, prefix, **kwargs):
+        t3_file = Path(str(prefix) + self.name + '_t3.csv')
+        np.savetxt(t3_file, self.t3, delimiter=',', **kwargs)
+
+    def write_states(self, prefix, **kwargs):
+        states_file = Path(str(prefix) + self.name + '_states.csv')
+        np.savetxt(states_file, self.states, delimiter=',', **kwargs)
+
+    def write_marks(self, prefix, **kwargs):
+        marks_file = Path(str(prefix) + self.name + '_marks.csv')
+        self.epigenmarks.to_csv(marks_file)
+
+    @property
+    def num_marks(self):
+        return len(self.epigenmarks)
 
     def __str__(self):
-        return f"{self.name} is a polymer with {self.num_beads} beads (each bead with " \
-               f"{self.num_nucleo_per_bead} nucleosomes)"
-
-
-
+        return f"Polymer<{self.name}, nbeads={self.num_beads}, " \
+               f"nmarks={self.num_marks}>"
--- tests/integration/mc.py
+++ tests/integration/mc.py
@@ -1,24 +1,23 @@
 """Tests that simply check if a simulation runs at all."""
 import chromo.mc as mc
-
-import numpy as np
-from chromo.Epigenmark import Epigenmark
 from chromo.Polymer import Polymer
-from chromo.MCmove import MCmove
 from chromo.Field import Field
 
 import numpy as np
-import pandas as pd
 
 from pathlib import Path
 integration_dir = Path(__file__).parent.absolute()
 
+
 def andy_mc():
     """
-    Original simulation, written by Andy. Should be replaced with a cleaner
-    version that doesn't rely on reading input files, except for optionally.
+    Original simulation, written by Andy.
+
+    Should be replaced with a cleaner version that doesn't rely on reading
+    input files, except for optionally.
     """
-    # Initialize the simulation by reading parameters from file (located in the input directory)
+    # Initialize the simulation by reading parameters from file (located in the
+    # input directory)
     input_dir = integration_dir / Path("input")
     input_file = input_dir / Path("sim_input")
     num_polymers = np.genfromtxt(input_file, comments='#', dtype=int)[0]
@@ -32,11 +31,13 @@ def andy_mc():
     num_bins_z = np.genfromtxt(input_file, comments='#', dtype=int)[8]
     num_save_mc = np.genfromtxt(input_file, comments='#', dtype=int)[9]
     num_mc_steps = np.genfromtxt(input_file, comments='#', dtype=int)[10]
-    from_file = np.genfromtxt(input_file, comments='#', dtype=bool)[11]
+    # from_file = np.genfromtxt(input_file, comments='#', dtype=bool)[11]
     output_dir = np.genfromtxt(input_file, comments='#', dtype=str)[12]
+    beads_per_polymer = np.genfromtxt(input_file, comments='#', dtype=str)[13]
     num_mc_move_types = 1
 
-    # Create the epigenetic marks from the Epigenmark class if "epigen_prop" files exist
+    # Create the epigenetic marks from the Epigenmark class if "epigen_prop"
+    # files exist
     epigenmarks = []
     prop_file = input_dir / Path("epigen_prop")
     if not prop_file.exists():
@@ -45,17 +46,16 @@ def andy_mc():
         epigenmarks.append(Epigenmark(epigenmark_count, input_dir))
 
     # Create the chromosomal polymers from the Polymer class
-    # 1. Define the sequence of epigenetic marks if "sequence" files exist for all epigenetic marks
-    # 2. Initialize the conformations from file (if from_file == True) or from random initiatlization
+    # 1. Define the sequence of epigenetic marks if "sequence" files exist for
+    #    all epigenetic marks
+    # 2. Initialize the conformations from file (if from_file == True) or from
+    #    random initiatlization
 
     # Create the polymers from the Polymer class if "chromo_prop" files exist
     polymers = []
-    prop_file = input_dir / Path("chromo_prop")
-    if not prop_file.exists():
-        raise OSError(f"Property file does not exist for chromosomes")
-    for chromo_count in range(1, num_polymers + 1):
-        polymers.append(Polymer(epigenmarks, num_epigenmark, chromo_count,
-                               length_bead, from_file, input_dir))
+    for chromo_count in range(num_polymers):
+        polymers.append(Polymer.straight_line_in_x(beads_per_polymer,
+                                                   length_bead))
 
     # Setup the Monte Carlo class to define the properties of the simulation
     mc_moves = np.arange(num_mc_move_types)
@@ -64,5 +64,3 @@ def andy_mc():
 
     return mc.polymer_in_field(polymers, epigenmarks, field, mc_moves,
                                num_mc_steps, num_save_mc, output_dir)
-
-
