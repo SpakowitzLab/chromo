f89598927b938264eaea37a691316af6586e7fbb
Joseph Wakim <jwakim@tower20.stanford.edu>
2020-08-27 20:45:31 -0700

    test in progress

--- tests/test_end_pivot_move.py
+++ tests/test_end_pivot_move.py
@@ -1,30 +1,21 @@
 
-from chromo.mc.mc_move import *
-import inputs.chromo_for_testing_mc_moves
-from chromo.mc.mc_sim import *
+from dataclasses import dataclass
+
+# from chromo.mc.mc_move import *
+from chromo.Polymer import *
 
 import numpy as np
 
 def test_end_pivot_move():
+    name = "test_end_pivot"
+    num_beads = 10
     
-    # Simulation Inputs
-    num_polymers = 1
-    length_bead = 55
-    num_epigenmark = 2
-    length_box_x = 3
-    num_bins_x = 3
-    length_box_y = 3 
-    num_bins_y = 3
-    length_box_z = 3 
-    num_bins_z = 3
-    num_save_mc = 10
-    num_mc_steps = 1000
-
-    # Epigenetic marks
-    epigenmark = []
+    states = np.zeros(num_beads)
+    length_per_bead = 1
+    polymer = Polymer.straight_line_in_x(name, marks, states, num_beads, length_per_bead)
+          
+    print(polymer.r)
+    input("breather")
     
-	# Generate objects
-	inputs.chromo_for_testing_mc_moves.main()
-
-	assert True
+    assert True
 
