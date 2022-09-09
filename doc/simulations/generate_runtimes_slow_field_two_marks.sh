64b6b18cca52a049e65cfcfe068c578b30e7809d
JosephWakim <programs@modistat.com>
2022-09-08 20:18:46 -0700

    deleted old analyses and resources in  folder

--- doc/simulations/generate_runtimes_slow_field_two_marks.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-python -m cProfile -s "cumulative" -o "rstats_two_marks_slow_field_1" mc_two_marks_for_runtimes_slow_field.py &
-python -m cProfile -s "cumulative" -o "rstats_two_marks_slow_field_2" mc_two_marks_for_runtimes_slow_field.py &
-python -m cProfile -s "cumulative" -o "rstats_two_marks_slow_field_3" mc_two_marks_for_runtimes_slow_field.py &
