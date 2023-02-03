8f09d6f8981fe24089a516386d58a79762f2c444
JosephWakim <programs@modistat.com>
2023-02-03 03:29:30 -0800

    added install of requirements-dev packages to make_all.sh

--- make_all.sh
+++ make_all.sh
@@ -3,7 +3,7 @@
 yes | conda create --name chromo python=3.9.12
 eval "$(conda shell.bash hook)"
 conda activate chromo
-yes | pip install -r requirements.txt -r requirements-dev.txt
+yes | pip install -r requirements.txt && pip install -r requirements-dev.txt
 pip install -e .
 yes | conda install pandoc
 cd docs
