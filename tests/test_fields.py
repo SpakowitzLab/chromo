a28ec7c49f08c9a473f9d6aae7f5107667a62fb4
JosephWakim <programs@modistat.com>
2023-02-02 23:35:03 -0800

    added test for determining neighboring voxels

--- tests/test_fields.py
+++ tests/test_fields.py
@@ -5,7 +5,7 @@ import pandas as pd
 import pytest
 
 from chromo.polymers import Chromatin
-from chromo.fields import UniformDensityField
+from chromo.fields import UniformDensityField, get_neighboring_bins
 import chromo.binders
 
 
@@ -57,3 +57,40 @@ def test_uniform_density_field_roundtrip():
         UniformDensityField.from_file(
             StringIO(udf.to_file(None)), [p], binders2
         )
+
+
+def test_get_neighbors_at_ind():
+    """For a 5-by-5-by-5 grid of voxels, we know what neighbors to expect.
+    """
+    nx = 5
+    ny = 5
+    nz = 5
+    nbr_bins = get_neighboring_bins(nx, ny, nz)
+
+    expected_neighbors_bin_12 = [
+        6, 7, 8, 11, 12, 13, 16, 17, 18,
+        106, 107, 108, 111, 112, 113, 116, 117, 118,
+        31, 32, 33, 36, 37, 38, 41, 42, 43
+    ]
+    for nbr_bin in expected_neighbors_bin_12:
+        assert nbr_bin in nbr_bins[12]
+
+    expected_neighbors_bin_0 = [
+        24, 20, 21, 4, 0, 1, 9, 5, 6,
+        29, 25, 26, 34, 30, 31, 49, 45, 46,
+        104, 100, 101, 109, 105, 106, 120, 121, 124
+    ]
+    for nbr_bin in expected_neighbors_bin_0:
+        assert nbr_bin in nbr_bins[0]
+
+    expected_neighbors_bin_24 = [
+        18, 19, 15, 23, 24, 20, 3, 4, 0,
+        28, 29, 25, 43, 44, 40, 48, 49, 45,
+        118, 119, 115, 123, 124, 120, 103, 104, 100
+    ]
+    for nbr_bin in expected_neighbors_bin_24:
+        assert nbr_bin in nbr_bins[24]
+
+    for i in nbr_bins.keys():
+        for j in nbr_bins[i]:
+            assert i in nbr_bins[j]
