11314b85fd8bf90f0f7ffd1bf46f7dbbf6945e1d
Angelika Hirsch <angelikahirsch21@gmail.com>
2023-09-11 17:59:05 -0700

    Testing mc sim function fix

--- submit_job.sh
+++ submit_job.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 $SBATCH --job-name=chromo
-#SBATCH --time=23:59:59
+#SBATCH --time=47:59:59
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
-#SBATCH --mem-per-cpu=8G
+#SBATCH --mem-per-cpu=16G
 source ~/.bashrc
 conda activate chromo
 cd /home/users/ahirsch1/Chromo_Github/chromo/chromo
 python run_simulation.py
 echo 'Job complete'#!/bin/bash
-o
+
