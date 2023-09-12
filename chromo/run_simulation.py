430dbb66614a1e2dadddd1e3da0f818488f930d8
Angelika Hirsch <angelikahirsch21@gmail.com>
2023-09-11 17:46:34 -0700

    changed number of snapshots

--- chromo/run_simulation.py
+++ chromo/run_simulation.py
@@ -112,7 +112,7 @@ moves_to_use = ctrl.all_moves_except_binding_state(
     controller=ctrl.SimpleControl
 )
 
-num_snapshots = 200
+num_snapshots = 10000
 # num_snapshots = 1000 # try 1000 and average for each set of 100, depending on pre-equilibration steps
 # count number of accepted moves for different conditions
 mc_steps_per_snapshot = 40000
