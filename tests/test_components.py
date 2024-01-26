a7014111d3a007b67e747e3fe31b0602314a28f2
JosephWakim <joewakim19@gmail.com>
2024-01-26 14:07:03 -0800

    added variable linker lengths to instantiation of polymer in test

--- tests/test_components.py
+++ tests/test_components.py
@@ -8,7 +8,7 @@ from chromo.polymers import Chromatin
 
 def test_polymer_saving():
     p = Chromatin.straight_line_in_x(
-        'Chr-1', 10, 1, states=np.zeros((10, 1), dtype=int),
+        'Chr-1', np.array([1.] * 9), states=np.zeros((10, 1), dtype=int),
         binder_names=np.array(['HP1']),
         chemical_mods=np.zeros((10, 1), dtype=int),
         chemical_mod_names=np.array(['H3K9me3'])
