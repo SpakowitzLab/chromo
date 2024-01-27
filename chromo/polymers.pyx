ab957235064d9b195e29fa45ec5fec4bfc37f307
JosephWakim <joewakim19@gmail.com>
2024-01-27 15:02:17 -0800

    removed restriction on range of delta_omega in sstwlc

--- chromo/polymers.pyx
+++ chromo/polymers.pyx
@@ -2062,10 +2062,11 @@ cdef class SSTWLC(SSWLC):
         delta_omega = omega - (
             self.bead_length[bond_ind] * NATURAL_TWIST_BARE / LENGTH_BP
         )
-        if delta_omega < 0:
-            delta_omega += 2 * np.pi * ((-delta_omega) // (2 * np.pi))
-        else:
-            delta_omega -= 2 * np.pi * (delta_omega // (2 * np.pi))
+        # Attempt simulation without restricting range of delta_omega
+        #if delta_omega < 0:
+        #    delta_omega += 2 * np.pi * ((-delta_omega) // (2 * np.pi))
+        #else:
+        #    delta_omega -= 2 * np.pi * (delta_omega // (2 * np.pi))
         E = (
             0.5 * self.eps_bend[bond_ind] * vec_dot3(bend, bend) +
             0.5 * self.eps_par[bond_ind] * (dr_par - self.gamma[bond_ind])**2 +
