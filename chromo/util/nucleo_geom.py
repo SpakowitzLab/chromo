corrected units of natural twist and added unit tests for nucleosome geometry

chromo/util/nucleo_geom.py
@@ -33,11 +33,11 @@ bp_wrap_default = 147
 # nm length of DNA wrapped around each nucleosome
 s_default = bp_wrap_default * LENGTH_BP
 # Natural twist of DNA (radians per bp)
-w0_default = 2 * np.pi / NUC_TWIST_DENS
+w0_default = 2 * np.pi / (NUC_TWIST_DENS * LENGTH_BP)
 # DNA length required for full (2pi) rotation around nucleosome
 Lt_default = np.sqrt(4 * np.pi**2 * R_default**2 + h_default**2)
 # Natural twist of wrapped DNA, correcting for helical torsion
-Phi_default = w0_default - 2 * np.pi * h_default / Lt_default**2
+Phi_default = w0_default - 2 * np.pi * h_default / (Lt_default**2)
 # Collection of geometric constants
 consts_dict = MappingProxyType({
     "R": R_default, "h": h_default, "bp_wrap": bp_wrap_default,

tests/test_nucleo_geom.py
@@ -2,95 +2,211 @@
 """
 
 import numpy as np
-from wlcstat.chromo import OmegaE2E
-from chromo.util.nucleo_geom import get_exiting_orientations, s_default
+from chromo.util.nucleo_geom import get_exiting_orientations, s_default, \
+    consts_dict, t3, t1, t2, normal
 from chromo.util.linalg import get_rotation_matrix
 
 
-def test_R_entry_exit():
-    """Test computation of the nucleosome entry/exit rotation matrix.
+def test_binormal():
+    """Test computation of the nucleosome binormal.
+    """
+    s = s_default
+    T3_vec = t3(s, consts_dict)
+    normal_vec = normal(s, consts_dict)
+    binormal_vec = np.cross(T3_vec, normal_vec)
+    assert np.allclose(np.linalg.norm(binormal_vec), 1), \
+        "Binormal vector should have unit norm."
+    assert np.allclose(np.dot(T3_vec, binormal_vec), 0), \
+        "Binormal vector should be orthogonal to T3 vector."
+    assert np.allclose(np.dot(normal_vec, binormal_vec), 0), \
+        "Binormal vector should be orthogonal to normal vector."
+    R = consts_dict["R"]
+    Lt = consts_dict["Lt"]
+    h = consts_dict["h"]
+    # The binormal is described analytically as:
+    binormal_check = np.array([
+        h / Lt * np.sin(2 * np.pi * s / Lt),
+        -h / Lt * np.cos(2 * np.pi * s / Lt),
+        2 * np.pi * R / Lt
+    ])
+    assert np.allclose(binormal_vec, binormal_check), \
+        "Binormal not consistent with theory."
+
+
+def test_T2_exit():
+    """Test computation of the exiting T2 vector.
+    """
+    # Define some arbitrary entering orientation
+    T3_enter = t3(0)
+    T1_enter = t1(0)
+    T2_enter = np.cross(T3_enter, T1_enter)
+    # Get the exiting orientations
+    _, T2_exit, _ = get_exiting_orientations(
+        s=s_default, T3_enter=T3_enter, T2_enter=T2_enter, T1_enter=T1_enter
+    )
+    # Compute the T2 exit vector another way:
+    T2_exit_2 = (
+        np.dot(t2(s=s_default), t3(s=0)) * T3_enter +
+        np.dot(t2(s=s_default), t2(s=0)) * T2_enter +
+        np.dot(t2(s=s_default), t1(s=0)) * T1_enter
+    )
+    print("T2_exit: ", T2_exit)
+    print("T2_exit_2: ", T2_exit_2)
+    assert np.allclose(T2_exit, T2_exit_2), \
+        "Error in computation of T2 vector."
+
+
+def test_get_rotation_matrix():
+    """Verify rotation matrix works between arbitrary vectors.
 
     Notes
     -----
-    The entry/exit rotation matrix provides the transformation matrix that
-    converts from the entering DNA orientation to the exiting DNA orientation.
+    The `get_rotation_matrix` function in the `linalg` module is used to
+    generate a rotation matrix that rotates two orthogonal vectors A and B
+    onto two other orthogonal vectors A' and B'. The test checks that the
+    method correctly performs the expected rotation.
     """
-
-    # Obtain the entry/exit rotation matrix from wlcstat
-    R_wlcstat = OmegaE2E()
-
-    # Obtain the rotation matrix from the nucleosome geometry module
-    # Start by defining some arbitrary entering orientation
-    T3_enter = np.array([1, 0, 0])
-    T1_enter = np.array([0, 1, 0])
-    T2_enter = np.cross(T3_enter, T1_enter)
-    # Now get the exiting orientations
+    # Define entering and exiting DNA tangents in the local frame
+    t3_0 = t3(0)
+    t1_0 = t1(0)
+    t2_0 = np.cross(t3_0, t1_0)
+    t3_1 = t3(s_default)
+    t1_1 = t1(s_default)
+    t2_1 = np.cross(t3_1, t1_1)
+    # Compute the rotation matrix that converts the entering orientation to the
+    # exiting orientation in the local frame
+    R_local = get_rotation_matrix(t3_0, t1_0, t3_1, t1_1)
+    # Rotate the entering DNA tangents to the exiting DNA tangents in the local
+    # frame
+    t3_1_check = np.dot(R_local, t3_0)
+    t1_1_check = np.dot(R_local, t1_0)
+    t2_1_check = np.dot(R_local, t2_0)
+    # Check that the rotated vectors are consistent with the exiting DNA
+    # tangents in the local frame
+    assert np.allclose(t3_1, t3_1_check), \
+        "Error rotating t3 vector using `get_rotation_matrix` in local frame."
+    assert np.allclose(t1_1, t1_1_check), \
+        "Error rotating t1 vector using `get_rotation_matrix` in local frame."
+    assert np.allclose(t2_1, t2_1_check), \
+        "Error rotating t2 vector using `get_rotation_matrix` in local frame."
+    # Generate new entry tangents arbitrarily in space
+    T3_entry = np.array([1, 0, 0])
+    T1_entry = np.array([0, 1, 0])
+    T2_entry = np.cross(T3_entry, T1_entry)
+    # Compute the exit tangents associated with the arbitrary entry tangents
     T3_exit, T2_exit, T1_exit = get_exiting_orientations(
-        s=s_default, T3_enter=T3_enter, T2_enter=T2_enter, T1_enter=T1_enter
+        s=s_default, T3_enter=T3_entry, T2_enter=T2_entry, T1_enter=T1_entry
     )
-    # Now get the rotation matrix
-    R_chromo = get_rotation_matrix(T3_enter, T2_enter, T3_exit, T2_exit)
-    assert np.allclose(np.dot(R_chromo, T3_enter), T3_exit), \
-        "R_chromo is incorrect. T3_enter does not map to T3_exit."
-    assert np.allclose(np.dot(R_chromo, T3_enter), T3_exit), \
-        "R_chromo is incorrect. T2_enter does not map to T2_exit."
+    # Compute the rotation matrix that converts the arbitrary entry tangents to
+    # the computed exit tangents in the global frame
+    R_global = get_rotation_matrix(T3_entry, T2_entry, T3_exit, T2_exit)
+    # Compute the exiting DNA orientations in the global frame using `R`
+    T3_exit_check = np.dot(R_global, T3_entry)
+    T2_exit_check = np.dot(R_global, T2_entry)
+    T1_exit_check = np.dot(R_global, T1_entry)
+    # Check that the computed exit tangents in the global frame are consistent
+    assert np.allclose(T3_exit, T3_exit_check), \
+        "Error obtaining T3_exit using `get_rotation_matrix` in global frame."
+    assert np.allclose(T2_exit, T2_exit_check), \
+        "Error obtaining T2_exit using `get_rotation_matrix` in global frame."
+    assert np.allclose(T1_exit, T1_exit_check), \
+        "Error obtaining T1_exit using `get_rotation_matrix` in global frame."
 
-    # Check if the two rotation matrices are the same
-    test_points = np.random.rand(10, 3)  # Generate random 3D points
-    transformed_points_1 = np.dot(R_wlcstat, test_points.T).T
-    transformed_points_2 = np.dot(R_chromo, test_points.T).T
 
-    # Check the angles between the transformed points
-    angles_wlcstat = np.array([
-        np.arctan2(  # Rotation about x-axis
-            R_wlcstat[2, 1], R_wlcstat[2, 2]
-        ),
-        np.arctan2(  # Rotation about y-axis
-            -R_wlcstat[2, 0], np.sqrt(R_wlcstat[2, 1]**2 + R_wlcstat[2, 2]**2)
-        ),
-        np.arctan2(  # Rotation about z-axis
-            R_wlcstat[1, 0], R_wlcstat[0, 0]
-        )
+def test_dot_product_matrix():
+    """Test that entry/exit tangents produce the same dot product matrix.
+
+    Notes
+    -----
+    The dot products between entering and exiting DNA tangents should produce
+    the same dot product matrix regardless of reference frame. This is because
+    the nucleosome is a fixed body -- the relationship between the entering
+    and exiting DNA tangents is invariant to the reference frame. This
+    relationship is captured by a 3x3 matrix produced from dot products of
+    different combinations of entering and exiting DNA tangents. This test
+    verifies that the dot product matrix is consistent in two reference frames.
+    """
+    # Define entering and exiting DNA tangents in the local frame
+    t3_0 = t3(0)
+    t1_0 = t1(0)
+    t2_0 = np.cross(t3_0, t1_0)
+    t3_1 = t3(s_default)
+    t1_1 = t1(s_default)
+    t2_1 = np.cross(t3_1, t1_1)
+    # Generate dot product matrix in the local frame
+    A_local = np.array([
+        [np.dot(t1_1, t1_0), np.dot(t1_1, t2_0), np.dot(t1_1, t3_0)],
+        [np.dot(t2_1, t1_0), np.dot(t2_1, t2_0), np.dot(t2_1, t3_0)],
+        [np.dot(t3_1, t1_0), np.dot(t3_1, t2_0), np.dot(t3_1, t3_0)]
     ])
-    angles_chromo = np.array([
-        np.arctan2(  # Rotation about x-axis
-            R_chromo[2, 1], R_chromo[2, 2]
-        ),
-        np.arctan2(  # Rotation about y-axis
-            -R_chromo[2, 0], np.sqrt(R_chromo[2, 1]**2 + R_chromo[2, 2]**2)
-        ),
-        np.arctan2(  # Rotation about z-axis
-            R_chromo[1, 0], R_chromo[0, 0]
-        )
+    # Generate new entry tangents arbitrarily in space
+    T3_0 = np.array([1, 0, 0])
+    T1_0 = np.array([0, 1, 0])
+    T2_0 = np.cross(T3_0, T1_0)
+    # Compute the exit tangents associated with the arbitrary entry tangents
+    T3_1, T2_1, T1_1 = get_exiting_orientations(
+        s=s_default, T3_enter=T3_0, T2_enter=T2_0, T1_enter=T1_0
+    )
+    # Generate dot product matrix in the global frame
+    A_global = np.array([
+        [np.dot(T1_1, T1_0), np.dot(T1_1, T2_0), np.dot(T1_1, T3_0)],
+        [np.dot(T2_1, T1_0), np.dot(T2_1, T2_0), np.dot(T2_1, T3_0)],
+        [np.dot(T3_1, T1_0), np.dot(T3_1, T2_0), np.dot(T3_1, T3_0)]
     ])
-
-    # Print Rotation matrices and points for debugging
-    print("Rotation Matrix from WLCSTAT:")
-    print(R_wlcstat)
-    print()
-    print("Rotation Matrix from Chromo:")
-    print(R_chromo)
-    print()
-    print("Original Points:")
-    print(test_points)
-    print()
-    print("Transformed Points from WLCSTAT:")
-    print(transformed_points_1)
-    print()
-    print("Transformed Points from Chromo:")
-    print(transformed_points_2)
-    print()
-    print("Angles from WLCSTAT:")
-    print(angles_wlcstat)
-    print()
-    print("Angles from Chromo:")
-    print(angles_chromo)
-
-    assert np.allclose(transformed_points_1, transformed_points_2), \
-        "The two rotation matrices are not the same."
-    assert np.allclose(angles_wlcstat, angles_chromo), \
-        "The two rotation matrices produce inconsistent angles."
+    # Check that the dot product matrices are consistent
+    assert np.allclose(A_local, A_global), \
+        "Error obtaining dot product matrix using `get_exiting_orientations`."
 
 
-if __name__ == "__main__":
-    test_R_entry_exit()
+def test_rotations_with_dot_products():
+    """Verify that dot product matrix produces the correct exit orientations.
+
+    Notes
+    -----
+    We calculate the exiting DNA orientations from the entering DNA orientations
+    using the dot product matrix. The entry/exit relationship can also be
+    captured by rotation matrices. We can compute the rotation matrix that
+    rotates the entering DNA orientation to the exiting DNA orientation in the
+    local frame. Call this rotation matrix R1. Then for an arbitrary entering
+    DNA orientation, we can rotate the orientation onto the local frame using
+    rotation matrix R2. We can then rotate the orientation onto the exiting
+    frame using rotation matrix R1. And finally, we can rotate back to the
+    global frame using the inverse of rotation matrix R2.
+
+    In this test, we compare the global exiting DNA orientations computed using
+    the dot product matrix with the global exiting DNA orientations computed
+    using the rotation matrices. We verify that the two methods produce the same
+    global exiting DNA orientations.
+    """
+    # Define entering and exiting DNA tangents in the local frame
+    t3_0 = t3(0)
+    t1_0 = t1(0)
+    t2_0 = np.cross(t3_0, t1_0)
+    t3_1 = t3(s_default)
+    t1_1 = t1(s_default)
+    t2_1 = np.cross(t3_1, t1_1)
+    # Generate dot product matrix in the local frame
+    R1 = get_rotation_matrix(t3_0, t2_0, t3_1, t2_1)
+    # Generate new entry tangents arbitrarily in space
+    T3_0 = np.array([1, 0, 0])
+    T1_0 = np.array([0, 1, 0])
+    T2_0 = np.cross(T3_0, T1_0)
+    # Compute the rotation matrix that rotates onto the local frame
+    R2 = get_rotation_matrix(T3_0, T2_0, t3_0, t2_0)
+    # Compute the exit tangents associated with the arbitrary entry tangents
+    # using the dot product matrix
+    T3_1, T2_1, T1_1 = get_exiting_orientations(
+        s=s_default, T3_enter=T3_0, T2_enter=T2_0, T1_enter=T1_0
+    )
+    # Compute the exit tangent associated with the arbitrary entry tangents
+    # using the rotation matrices
+    T3_1_check = np.dot(np.linalg.inv(R2), np.dot(R1, np.dot(R2, T3_0)))
+    T2_1_check = np.dot(np.linalg.inv(R2), np.dot(R1, np.dot(R2, T2_0)))
+    T1_1_check = np.dot(np.linalg.inv(R2), np.dot(R1, np.dot(R2, T1_0)))
+    # Check that the exit tangents are consistent
+    assert np.allclose(T3_1, T3_1_check), \
+        "Error obtaining T3_exit using dot product matrix."
+    assert np.allclose(T2_1, T2_1_check), \
+        "Error obtaining T2_exit using dot product matrix."
+    assert np.allclose(T1_1, T1_1_check), \
+        "Error obtaining T1_exit using dot product matrix."